bae4bda9cff33141a66d73c12be546bfa59dafb1
HMM Thasneem <137602501+thasneem36@users.noreply.github.com>
2025-03-08 15:50:31 +0530

    nav update version

--- src/navBar/Header.js
+++ src/navBar/Header.js
@@ -8,17 +8,21 @@ function Header(){
             <nav>
                 <div className="heroSection">
                     <img src={ICSTlogo} />
-                    <p>ICST HR System</p>
+                    <p>HR System</p>
                 </div>
                 <div className="navLinks">
                     <ul>
-                        <li>Home</li>
-                        <li>Home</li>
-                        <li>Home</li>
-                        <li>Home</li>
+                        <li>Dashboard</li>
+                        <li>Manage Employees</li>
+                        <li>Attendance Management</li>
+                        <li>Leave Management</li>
+                        <li>Payroll Management</li>
+                        <li>User Roles</li>
+                        <li>Reports & Analytics</li>
+                        <li>Settings</li>
                     </ul>
                 </div>
-                <div>
+                <div className="logout">
                     <a>Logout</a>
                 </div>
             </nav>
